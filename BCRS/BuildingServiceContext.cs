9c7d91ad231a6e89959940d4fcd0d54641b99756
Nazar32 <nazar3223@gmail.com>
2016-12-03 20:09:39 +0200

    added migration for roles

---
+++ BCRS/BuildingServiceContext.cs
@@ -0,0 +1,29 @@
+﻿using BCRS.Migrations;
+using BCRS.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Web;
+
+namespace BCRS
+{
+    public class BuildingServiceContext : DbContext
+    {
+        public BuildingServiceContext() : base("name=BuildingServiceContext")
+        {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<BuildingServiceContext, Configuration>("BuildingServiceContext"));
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.Add(new EntityTypeConfiguration<Role>());
+            //modelBuilder.Configurations.Add(new EntityTypeConfiguration<User>());
+            //modelBuilder.Configurations.Add(new EntityTypeConfiguration<Company>());
+            //modelBuilder.Configurations.Add(new EntityTypeConfiguration<Building>());
+            //modelBuilder.Configurations.Add(new EntityTypeConfiguration<Rating>());
+            //modelBuilder.Configurations.Add(new EntityTypeConfiguration<Comment>());
+        }
+    }
+}
