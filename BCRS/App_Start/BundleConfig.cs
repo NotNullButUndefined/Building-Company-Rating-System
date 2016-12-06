565c1b863721563ab45af43728e562ab95f4c668
hoflener <egocoe.321@gmail.com>
2016-12-06 23:24:21 +0200

    home page marking and style, bundles added

---
+++ BCRS/App_Start/BundleConfig.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace BCRS.App_Start
+{
+    public class BundleConfig
+    {
+
+    
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/bundles/css").Include("~/Content/styles/itera.css",
+                                                                  "~/Content/styles/searchStyles.css",
+                                                                   "~/Content/styles/font-awesome.min.css"));
+        }
+    }
+    }
